4ec12737c77f6eab74edfb9e8d066c2181c19c20
samchapman342 <60855297+samchapman342@users.noreply.github.com>
2020-06-09 20:04:09 +0100

    first 3

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const botsettings = require('./botsettings.json')
 const Discord = require("discord.js")
 const client = new Discord.Client();
 const PREFIX = "!"
@@ -9,8 +10,8 @@ client.on('ready', async() => {
 require("./util/eventHandler")(client)
 
 const fs = require("fs");
-bot.commands = new Discord.Collection();
-bot.aliases = new Discord.Collection();
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 
 fs.readdir("./commands", (err, files) => {
 
@@ -23,28 +24,28 @@ fs.readdir("./commands", (err, files) => {
 
     jsfile.forEach((f, i) => {
         let pull = require(`./commands/${f}`);
-        bot.commands.set(pull.config.name, pull);
+        client.commands.set(pull.config.name, pull);
         pull.config.aliases.forEach(alias => {
-            bot.aliases.set(alias, pull.config.name)
+            client.aliases.set(alias, pull.config.name)
         })
     })
 });
 
-bot.on("message", async message => {
-    if(message.author.bot || message.channel.type === "dm") return;
+client.on("message", async message => {
+    if(message.author.client || message.channel.type === "dm") return;
 
-    let prefix = botsettings.prefix;
+    let prefix = clientsettings.prefix;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
 
     if(!message.content.startsWith(prefix)) return;
-    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+    let commandfile = client.commands.get(cmd.slice(prefix.length)) || client.commands.get(client.aliases.get(cmd.slice(prefix.length)))
 })
 
 
 
-bot.login(botsettings.token);
+client.login(botsettings.token);
 
 
 
--- util/eventHandler.js
+++ util/eventHandler.js
@@ -1,4 +1,4 @@
-const reqEvent = (event) => require(`../events/${event}`)
+const reqEvent = (event) => require(`../events/${event}.js`)
 
 module.exports = bot => {
     bot.on("ready", function() {reqEvent("ready") (bot) });
