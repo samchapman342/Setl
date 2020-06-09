b472799dea8db011e39e150c4d6e63ba55b45d98
samchapman342 <60855297+samchapman342@users.noreply.github.com>
2020-06-09 23:50:05 +0100

    ............

--- index.js
+++ index.js
@@ -12,23 +12,27 @@ require("./util/eventHandler")(client)
 
 const fs = require("fs");
 client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
-
     if(err) console.log(err)
 
     let jsfile = files.filter(f => f.split(".").pop() === "js") 
     if(jsfile.length <= 0) {
-         return console.log("[LOGS] Couldn't Find Commands!");
+         console.log("[LOGS] Couldn't Find Commands!");
+         return;
     }
+
+    console.log(`Loading commands!`);
 
     jsfile.forEach((f, i) => {
         let pull = require(`./commands/${f}`);
-        client.commands.set(pull.config.name, pull);  
-        pull.config.aliases.forEach(alias => {
-            client.aliases.set(alias, pull.config.name)
-        });
+        console.log(`Command "${f}" loaded!`)
+        client.commands.set(pull.config.name, pull);
+        if(pull.config.aliases) {
+          pull.config.aliases.forEach(alias => {
+            client.commands.set(alias, pull)
+          })
+        }
     });
 });
 
