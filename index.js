6665d4a349f355e48786da3b167d834fe6fc398c
samchapman342 <60855297+samchapman342@users.noreply.github.com>
2020-06-09 23:25:49 +0100

    ../.

--- index.js
+++ index.js
@@ -13,23 +13,22 @@ const fs = require("fs");
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 
-fs.readdir("./commands", (err, files) => {
+fs.readdir("./commands/", (err, files) => {
 
     if(err) console.log(err)
 
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
     if(jsfile.length <= 0) {
-        return console.log("[LOGS] Couldn't Find Commands!");
+         return console.log("[LOGS] Couldn't Find Commands!");
     }
-
 
     jsfile.forEach((f, i) => {
         let pull = require(`./commands/${f}`);
-        client.commands.set(pull.config.name, pull);
+        client.commands.set(pull.config.name, pull);  
         pull.config.aliases.forEach(alias => {
             client.aliases.set(alias, pull.config.name)
-        })
-    })
+        });
+    });
 });
 
 client.on("message", async message => {
