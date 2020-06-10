e65b609c42b8150ce1862bf970b796d2813b7e94
samchapman342 <60855297+samchapman342@users.noreply.github.com>
2020-06-10 16:40:35 +0100

    gh

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,10 +1,38 @@
-module.exports.run = async (bot, message, args) => {
-    let epicRole = message.guild.roles.cache.get('ROLE_ID_HERE');
-    const member = message.mentions.members.first();
+const checkPermissionRole = (role) => role.permissions.has('ADMINISTRATOR') || role.permissions.has('KICK_MEMBERS') || role.permissions.has('BAN_MEMBERS') || role.permissions.has('MANAGE_GUILD') || role.permissions.has('MANAGE_CHANNELS');
 
-    member.roles.add(epicRole);
-    message.channel.send('Role Added')
+module.exports = {
+    run: async(client, message, args) => {
+        let roleNames = args.split(", ");
+        let roleSet = new Set(roleNames);
+        let { cache } = message.guild.roles;
+        roleSet.forEach(roleName => {
+            let role = cache.find(role => role.name.toLowerCase() === roleName.toLowerCase());
+            if(role) {
+                if(message.member.roles.cache.has(role.id)) {
+                    message.channel.send("You already have this role!");
+                    return;
+                }
+                if(checkPermissionRole(role)) {
+                    message.channel.send("You cannot add yourself to this role.");
+                }
+                else {
+                    message.member.roles.add(role)
+                        .then(member => message.channel.send("You were added to this role!"))
+                        .catch(err => {
+                            console.log(err);
+                            message.channel.send("Something went wrong...");
+                        });
+                }
+            }
+            else {
+                message.channel.send("Role not found!");
+            }
+        });
+    },
+    aliases: ['roleadd'],
+    description: 'Adds a role to a Guild Member'
 }
+
 
 
 module.exports.config = {
@@ -12,5 +40,5 @@ module.exports.config = {
     description: "adds role",
     usage: "!addrole",
     accessableby: "addrole",
-    aliases: ['ar', 'addrole']
+    aliases: ['ar', 'addsrole']
 }
